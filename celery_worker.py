d9554458ccd7c768717151309b8ee7da69995db0
Oqba Jawed <106262062+Oqba-Jawed@users.noreply.github.com>
2024-06-04 02:00:55 +0500

    Update celery_worker.py

--- celery_worker.py
+++ celery_worker.py
@@ -3,7 +3,8 @@ from celery.schedules import crontab
 from imports import *
 from utils import Utility
 
-app = Celery("worker", broker="redis://localhost:6379/0", include=['celery_worker'])
+app = Celery("worker", broker="redis://redis:6379/0", include=['celery_worker'])
+
 
 app.conf.beat_schedule = {
     'call-save-all-products-every-hour': {
@@ -15,7 +16,8 @@ app.conf.beat_schedule = {
 @app.task
 def call_periodic_task():
     # Example function that gets called
-    category_response = requests.post("http://localhost:3000/list-categories")
-    products_response = requests.post("http://localhost:3000/save-all-products")
+    print("execution started")
+    category_response = requests.post("http://web:3000/list-categories")
+    products_response = requests.post("http://web:3000/save-all-products")
     update_vectorstore = Utility.get_vectorstore(True)
     print("Periodic task executed")
